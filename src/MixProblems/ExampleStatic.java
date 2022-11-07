650fd27ceaa8bf49070bd6506b1fd7f74e62fef1
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2022-11-06 23:20:02 -0500

    static

--- src/MixProblems/ExampleStatic.java
+++ src/MixProblems/ExampleStatic.java
@@ -3,17 +3,16 @@ package MixProblems;
 public class ExampleStatic {
     public static void main(String[] args) {
         ExampleStatic obj1 = new ExampleStatic();
-        print();
-        obj1.print();
-        doubler(3);
         System.out.println("obj1.doubler(3) = " + obj1.doubler(3));
+        print();
+        obj1.print(); // bad practice
     }
 
     public static void print() {
         System.out.println("print() method");
     }
 
-    public static int doubler(int n) {
+    public int doubler(int n) {
         return 2*n;
     }
 }
